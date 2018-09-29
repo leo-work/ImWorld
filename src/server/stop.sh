54a0609d38d2d01fc0f9bd4eb78b504f78e1ffd4
leo <16241022@qq.com>
2018-09-29 11:11:46 +0800

    更新

--- src/server/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-bin="./bin/run"
-PID=`ps -eopid,cmd | grep "run" |  grep -v "grep"|grep -v "sh" |awk '{print $1}'`
-echo $PID
-if [ "$PID" != "" ]; then
-        kill -9 $PID
-            echo "kill -9 $PID"
-fi
